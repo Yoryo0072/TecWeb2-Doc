4632ef4f372ae628f4f7a2e9d318d7613bce1b72
Yoryo0072 <giorsebas0772.gslp@outlook.com>
2024-04-16 11:35:27 -0400

    comentarios

---
+++ php/comentarios.php
@@ -0,0 +1,11 @@
+<?php
+// Esto es un comentario de una línea
+echo "Frase 1";
+
+# Esto es otro comentario de una línea
+echo "Frase 2";
+
+/* Este comentario
+es un comentario de múltiples líneas */
+echo "Frase 3";
+?>
