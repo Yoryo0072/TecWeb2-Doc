351f0d397c1a6d96aca6a4f52cab1637f2153cc0
Yoryo0072 <giorsebas0772.gslp@outlook.com>
2024-04-16 11:31:38 -0400

    commit php

---
+++ php/sintaxis.php
@@ -0,0 +1,7 @@
+<?php
+/* Etiquetas de apertura y cierre de PHP
+El código PHP se encierra entre las etiquetas de apertura y cierre: <?php  ?>. Todo el código que se encuentre
+estas dos etiquetas será interpretado como código PHP. */
+
+echo "Hola Mundo, con PHP";
+?>
